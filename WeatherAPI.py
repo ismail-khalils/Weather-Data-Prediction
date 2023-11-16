fd17420e21b4e9b63a87d6b94eb36bce5f2a66d9
Ismail Khalil <ikhali4@wgu.edu>
2023-11-16 15:35:46 -0500

    Fixed WeatherAPI to include last 5 years method

--- WeatherAPI.py
+++ WeatherAPI.py
@@ -1,7 +1,8 @@
 import requests
 import json
+from datetime import datetime, timedelta
 
-#Creating the WeatherAPI class to get the data from the weather api on my specified day and five years before
+#Creating the WeatherAPI class and including a method to get the data for the past five years on my chose date
 class WeatherAPI:
     def __init__(self):
         self.api = 'https://archive-api.open-meteo.com/v1/archive?latitude=38.658707&longitude=-77.257919&start_date=2018-07-04&end_date=2023-07-04&daily=temperature_2m_mean,precipitation_sum,wind_speed_10m_max&temperature_unit=fahrenheit&wind_speed_unit=mph&precipitation_unit=inch&timezone=America%2FNew_York'
@@ -19,4 +20,14 @@ class WeatherAPI:
     def get_precipitation_sum(self):
         precipitation_sum = self.data['daily']['precipitation_sum']
         return precipitation_sum
+
+    def get_past_five_years_data(self):
+        past_data = []
+        for i in range(1, 6):
+            past_date = (datetime.strptime('2023-07-04', "%Y-%m-%d") - timedelta(days=i*365)).strftime("%Y-%m-%d")
+            self.api = 'https://archive-api.open-meteo.com/v1/archive?latitude=38.658707&longitude=-77.257919&start_date=' + past_date + '&end_date=' + past_date + '&daily=temperature_2m_mean,precipitation_sum,wind_speed_10m_max&temperature_unit=fahrenheit&wind_speed_unit=mph&precipitation_unit=inch&timezone=America%2FNew_York'
+            response = requests.get(self.api)
+            past_data.append(json.loads(response.text))
+        return past_data
+
 
