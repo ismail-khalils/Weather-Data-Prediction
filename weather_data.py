73a4bb1c41e62b2b8e29fb65e5b6ab9e8c33b422
Ismail Khalil <ikhali4@wgu.edu>
2023-11-17 21:58:49 -0500

    Added repr method

--- weather_data.py
+++ weather_data.py
@@ -18,3 +18,10 @@ class WeatherData:
         self.min_precipitation = min_precipitation,
         self.max_precipitation = max_precipitation
 
+    def __repr__(self):
+        return (f"<WeatherTable(location_latitude={self.location_latitude}, location_longitude={self.location_longitude},"
+                f" month={self.month}, day_of_month={self.day_of_month}, year={self.year}, avg_temp={self.avg_temp}, "
+                f"min_temp={self.min_temp}, max_temp={self.max_temp}, avg_wind_speed={self.avg_wind_speed}, "
+                f"min_wind_speed={self.min_wind_speed}, max_wind_speed={self.max_wind_speed}, "
+                f"sum_precipitation={self.sum_precipitation}, min_precipitation={self.min_precipitation}, "
+                f"max_precipitation={self.max_precipitation})>")
